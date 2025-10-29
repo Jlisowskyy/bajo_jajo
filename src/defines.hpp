0458a25e415ea3b946fab84bcfaf9815f19d18bc
Jakub Lisowski <Jlisowskyy@gmail.com>
2025-10-30 00:11:11 +0100

    Added initial graph struct

---
+++ src/defines.hpp
@@ -0,0 +1,7 @@
+#ifndef DEFINES_HPP
+#define DEFINES_HPP
+
+#define NODISCARD [[nodiscard]]
+#define FUNC_INLINE inline __attribute__((always_inline))
+
+#endif //DEFINES_HPP
---
+++ src/graph.hpp
@@ -0,0 +1,43 @@
+#ifndef GRAPH_HPP
+#define GRAPH_HPP
+
+#include "defines.hpp"
+
+#include <cstdint>
+
+class Graph
+{
+    public:
+    explicit Graph(const std::uint32_t num_vertices): vertices_(num_vertices)
+    {
+        neighbourhood_matrix_ = new std::uint32_t[num_vertices*num_vertices]{};
+    }
+
+    void AddEdge(const std::uint32_t u, const std::uint32_t v)
+    {
+        GetEdges(u, v) += 1;
+    }
+
+    NODISCARD FUNC_INLINE const std::uint32_t& GetEdges(const std::uint32_t u, const std::uint32_t v) const
+    {
+        return neighbourhood_matrix_[u*vertices_ + v];
+    }
+
+    NODISCARD FUNC_INLINE std::uint32_t& GetEdges(const std::uint32_t u, const std::uint32_t v)
+    {
+        return neighbourhood_matrix_[u*vertices_ + v];
+    }
+
+     NODISCARD FUNC_INLINE std::uint32_t GetVertices() const
+    {
+        return vertices_;
+    }
+
+    private:
+    std::uint32_t vertices_{};
+    std::uint32_t* neighbourhood_matrix_{};
+};
+
+Graph ParseGraph(const char* file*);
+
+#endif //GRAPH_HPP
